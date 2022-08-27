128216e5e8dd8196bba0eee2d3dc73d792f62465
Jiri Zboril <jirkazboril1997@gmail.com>
2022-08-27 16:04:11 +0200

    Changed whole thing to threads

--- Auctioner.py
+++ Auctioner.py
@@ -23,18 +23,26 @@ th.start()
 print("Auctioner started. Press esc to end")
 
 while end_game == False:
+    found = False
     pounder = gui.locateCenterOnScreen(img_dir + "18pounder-004.png")
     if pounder:
+        found = True
         gui.leftClick(pounder.x + 267, pounder.y + 14)
     pounder = gui.locateCenterOnScreen(img_dir + "18pounder-003.png")
     if pounder:
+        found = True
         gui.leftClick(pounder.x + 267, pounder.y + 14)
 
     harpoon = gui.locateCenterOnScreen(img_dir + "4harpoon-004.png")
     if harpoon:
+        found = True
         gui.leftClick(harpoon.x + 267, harpoon.y + 14)
     harpoon = gui.locateCenterOnScreen(img_dir + "4harpoon-003.png")
     if harpoon:
+        found = True
         gui.leftClick(harpoon.x + 267, harpoon.y + 14)
+
+    if not found:
+        gui.leftClick(273,190)
 
 print("Auctioner ended.")
