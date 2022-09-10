075f89d87718392bb086f52717472f6814f3deb6
Jiri Zboril <jirkazboril1997@gmail.com>
2022-09-10 16:40:20 +0200

    Smugglers Auctioner created and functional

--- Auctioner_Smugglers_Auction.py
+++ Auctioner_Smugglers_Auction.py
@@ -49,10 +49,10 @@ while end_game == False:
         gui.leftClick(moonlight_figurehead.x + 267, moonlight_figurehead.y + 14)
 
     # Moonforged Anchor Weights
-    moonforged_anchor = gui.locateCenterOnScreen(img_dir + "moonforged_anchor_weights_auction_time.png")
-    if moonforged_anchor:
-        found = True
-        gui.leftClick(moonforged_anchor.x + 267, moonforged_anchor.y + 14)
+    #moonforged_anchor = gui.locateCenterOnScreen(img_dir + "moonforged_anchor_weights_auction_time.png")
+    #if moonforged_anchor:
+    #    found = True
+    #    gui.leftClick(moonforged_anchor.x + 267, moonforged_anchor.y + 14)
 
     # Blood moon bulkhead
     #bloodmoon_bulkhead = gui.locateCenterOnScreen(img_dir + "blood_moon_bulkhead_auction_time.png")
